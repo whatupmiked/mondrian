5730e9681a54f4cb9051554f23291627851f4c3c
whatupmiked <whatupmiked@gmail.com>
2017-06-27 01:02:31 +0100

    Reformatted canvas file to try to deal with redrawing issue

--- mondrian.no.2.js
+++ mondrian.no.2.js
@@ -1,50 +1,60 @@
 var c2 = document.getElementById("no2");
 
 var verticalOne = c2.getContext("2d");
+var horizontalOne = c2.getContext("2d");
+var horizontalTwo = c2.getContext("2d");
+var horizontalThree = c2.getContext("2d");
+var regionOne = c2.getContext("2d");
+var regionTwo = c2.getContext("2d");
+var regionThree = c2.getContext("2d");
+var regionFour = c2.getContext("2d");
+var regionFive = c2.getContext("2d");
+var regionSix = c2.getContext("2d")
+
+// Create the first vertial line from left to right
+verticalOne.beginPath();
 verticalOne.moveTo(140,0);
 verticalOne.lineTo(140,200);
 verticalOne.lineWidth=3; <!--to increase the width of the stroke-->
 verticalOne.stroke();
 
-var horizontalOne = c2.getContext("2d");
+// Create the first horizontal line from top to bottom
+horizontalOne.beginPath();
 horizontalOne.moveTo(0,60);
 horizontalOne.lineTo(200,60);
 horizontalOne.stroke();
 
-var horizontalTwo = c2.getContext("2d");
+// Create the second horizontal line from top to bottom
+horizontalTwo.beginPath();
 horizontalTwo.moveTo(140,120);
 horizontalTwo.lineTo(200,120);
 horizontalTwo.stroke();
 
-var horizontalThree = c2.getContext("2d");
+// Create the third horizontal line from top to bottom
+horizontalThree.beginPath();
 horizontalThree.moveTo(0,158);
 horizontalThree.lineTo(140,158);
 horizontalThree.stroke();
 
-var regionOne = c2.getContext("2d");
 regionOne.fillStyle = "#FF0000";
 regionOne.fillRect(0,0,140,60);
 
-var regionTwo = c2.getContext("2d");
 regionOne.fillStyle = "#ffffff";
 regionOne.fillRect(140,0,60,60);
 
-var regionThree = c2.getContext("2d");
 regionThree.fillStyle = "#ffffff";
 regionThree.fillRect(0,60,140,98);
 
-var regionFour = c2.getContext("2d");
 regionFour.fillStyle = "#01579B";
 regionFour.fillRect(140,60,60,60);
 
-var regionFive = c2.getContext("2d");
 regionFive.fillStyle = "#ffffff";
 regionFive.fillRect(140,120,60,80);
 
-var regionSix = c2.getContext("2d")
 regionSix.fillStyle = "#ffc300"
 regionSix.fillRect(0,160,140,60)
 
+// Extra blank lines to deal with layering issues
 var sLast = c2.getContext("2d");
 sLast.moveTo(0,0);
 sLast.lineTo(0,0);
